5e25f15dab7335bbc68e0c37ccbe02c467711b67
Aimen SIJOUMI <asijoumi@wewyse.com>
2019-11-15 06:51:22 +0100

    add text buttons to variables (to be translated in other languages)

--- src/jw-pagination.component.ts
+++ src/jw-pagination.component.ts
@@ -7,19 +7,19 @@ import paginate = require('jw-paginate');
   selector: 'jw-pagination',
   template: `<ul *ngIf="pager.pages && pager.pages.length" class="pagination">
   <li [ngClass]="{disabled:pager.currentPage === 1}" class="page-item first-item">
-      <a (click)="setPage(1)" class="page-link">First</a>
+      <a (click)="setPage(1)" class="page-link">{{ firstItemButton }}</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === 1}" class="page-item previous-item">
-      <a (click)="setPage(pager.currentPage - 1)" class="page-link">Previous</a>
+      <a (click)="setPage(pager.currentPage - 1)" class="page-link">{{ previousItemButton }}</a>
   </li>
   <li *ngFor="let page of pager.pages" [ngClass]="{active:pager.currentPage === page}" class="page-item number-item">
       <a (click)="setPage(page)" class="page-link">{{page}}</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === pager.totalPages}" class="page-item next-item">
-      <a (click)="setPage(pager.currentPage + 1)" class="page-link">Next</a>
+      <a (click)="setPage(pager.currentPage + 1)" class="page-link">{{ nextItemButton }}</a>
   </li>
   <li [ngClass]="{disabled:pager.currentPage === pager.totalPages}" class="page-item last-item">
-      <a (click)="setPage(pager.totalPages)" class="page-link">Last</a>
+      <a (click)="setPage(pager.totalPages)" class="page-link">{{ lastItemButton }}</a>
   </li>
 </ul>`
 })
@@ -30,6 +30,10 @@ export class JwPaginationComponent implements OnInit, OnChanges {
   @Input() initialPage = 1;
   @Input() pageSize = 10;
   @Input() maxPages = 10;
+  @Input() firstItemButton = "First";
+  @Input() previousItemButton = "Previous";
+  @Input() nextItemButton = "Next";
+  @Input() lastItemButton = "Last";
 
   pager: any = {};
 
